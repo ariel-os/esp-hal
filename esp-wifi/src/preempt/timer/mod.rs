e09e1a0aadcb01f0c97859ea2cab291a8163ab24
Kaspar Schleiser <kaspar@schleiser.de>
2025-02-07 10:27:10 +0100

    fixup! esp-wifi: split `timer` into `preempt::timer`, `radio` and `time`

--- esp-wifi/src/preempt/timer/mod.rs
+++ esp-wifi/src/preempt/timer/mod.rs
@@ -1,7 +1,7 @@
 use esp_hal::sync::Locked;
 
-#[cfg_attr(any(esp32, esp32s2, esp32s3), path = "xtensa.rs")]
-#[cfg_attr(any(esp32c2, esp32c3, esp32c6, esp32h2), path = "riscv.rs")]
+#[cfg_attr(xtensa, path = "xtensa.rs")]
+#[cfg_attr(riscv, path = "riscv.rs")]
 mod arch_specific;
 
 pub(crate) use arch_specific::*;
