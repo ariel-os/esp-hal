aa8d1995fc06eb5e6d04a2022ecf3679b4929e63
Kaspar Schleiser <kaspar@schleiser.de>
2025-02-07 14:38:17 +0100

    fixup! change to "extern trait impl" scheme

--- esp-wifi/src/preempt_extern.rs
+++ esp-wifi/src/preempt_extern.rs
@@ -18,9 +18,9 @@
 ///
 /// esp_wifi::scheduler_impl!(static SCHEDULER: MyScheduler = MyScheduler {});
 /// ```
-use crate::binary::c_types;
+use core::ffi::c_void;
 
-trait Scheduler: Send + Sync + 'static {
+pub trait Scheduler: Send + Sync + 'static {
     /// This function is called by `esp-wifi` when starting up the WiFi stack.
     /// The passed `timer` can be used for e.g., implementing a periodic timer.
     fn setup(&self, timer: crate::TimeBase);
@@ -34,41 +34,41 @@ trait Scheduler: Send + Sync + 'static {
     /// This function is called by threads and should return an opaque handle
     /// for the calling thread. The same handle will be passed to
     /// `esp_wifi_preempt_schedule_task_deletion`.
-    fn current_task(&self) -> *mut c_types::c_void;
+    fn current_task(&self) -> *mut c_void;
 
     /// This function is used to create threads.
     /// It should allocate the stack.
     fn task_create(
         &self,
-        task: extern "C" fn(*mut c_types::c_void),
-        param: *mut c_types::c_void,
+        task: extern "C" fn(*mut c_void),
+        param: *mut c_void,
         task_stack_size: usize,
-    ) -> *mut c_types::c_void;
+    ) -> *mut c_void;
 
     /// This function is called to let the scheduler now this thread is not
     /// needed anymore and should be deleted. After this function is called,
     /// the thread should not be scheduled anymore. The thread stack can be
     /// free'ed.
-    fn schedule_task_deletion(task_handle: *mut c_types::c_void);
+    fn schedule_task_deletion(&self, task_handle: *mut c_void);
 
     /// This function should return an opaque per-thread pointer to an
     /// usize-sized memory location, which will be used to store a pointer
     /// to a semaphore for this thread.
-    fn current_task_thread_semaphore(&self) -> *mut c_types::c_void;
+    fn current_task_thread_semaphore(&self) -> *mut c_void;
 }
 
 extern "Rust" {
     fn esp_wifi_preempt_setup(timer: crate::TimeBase);
     fn esp_wifi_preempt_disable();
     fn esp_wifi_preempt_yield_task();
-    fn esp_wifi_preempt_current_task() -> *mut c_types::c_void;
+    fn esp_wifi_preempt_current_task() -> *mut c_void;
     fn esp_wifi_preempt_task_create(
-        task: extern "C" fn(*mut c_types::c_void),
-        param: *mut c_types::c_void,
+        task: extern "C" fn(*mut c_void),
+        param: *mut c_void,
         task_stack_size: usize,
-    ) -> *mut c_types::c_void;
-    fn esp_wifi_preempt_schedule_task_deletion(task_handle: *mut c_types::c_void);
-    fn esp_wifi_preempt_current_task_thread_semaphore() -> *mut c_types::c_void;
+    ) -> *mut c_void;
+    fn esp_wifi_preempt_schedule_task_deletion(task_handle: *mut c_void);
+    fn esp_wifi_preempt_current_task_thread_semaphore() -> *mut c_void;
 }
 
 pub(crate) fn setup(timer: crate::TimeBase) {
@@ -83,23 +83,23 @@ pub(crate) fn yield_task() {
     unsafe { esp_wifi_preempt_yield_task() }
 }
 
-pub(crate) fn current_task() -> *mut c_types::c_void {
+pub(crate) fn current_task() -> *mut c_void {
     unsafe { esp_wifi_preempt_current_task() }
 }
 
 pub(crate) fn task_create(
-    task: extern "C" fn(*mut c_types::c_void),
-    param: *mut c_types::c_void,
+    task: extern "C" fn(*mut c_void),
+    param: *mut c_void,
     task_stack_size: usize,
-) -> *mut c_types::c_void {
+) -> *mut c_void {
     unsafe { esp_wifi_preempt_task_create(task, param, task_stack_size) }
 }
 
-pub(crate) fn schedule_task_deletion(task_handle: *mut c_types::c_void) {
+pub(crate) fn schedule_task_deletion(task_handle: *mut c_void) {
     unsafe { esp_wifi_preempt_schedule_task_deletion(task_handle) }
 }
 
-pub(crate) fn current_task_thread_semaphore() -> *mut c_types::c_void {
+pub(crate) fn current_task_thread_semaphore() -> *mut c_void {
     unsafe { esp_wifi_preempt_current_task_thread_semaphore() }
 }
 
@@ -112,37 +112,36 @@ macro_rules! scheduler_impl {
         static $name: $t = $val;
 
         #[no_mangle]
-        fn esp_wifi_preempt_setup(&self, timer: crate::TimeBase) {
-            <$t as $crate::Scheduler>::setup(&$name)
+        fn esp_wifi_preempt_setup(timer: $crate::TimeBase) {
+            <$t as $crate::preempt::Scheduler>::setup(&$name, timer)
         }
         #[no_mangle]
-        fn esp_wifi_preempt_disable(&self) {
-            <$t as $crate::Scheduler>::disable(&$name)
+        fn esp_wifi_preempt_disable() {
+            <$t as $crate::preempt::Scheduler>::disable(&$name)
         }
         #[no_mangle]
-        fn esp_wifi_preempt_yield_task(&self) {
-            <$t as $crate::Scheduler>::yield_task(&$name)
+        fn esp_wifi_preempt_yield_task() {
+            <$t as $crate::preempt::Scheduler>::yield_task(&$name)
         }
         #[no_mangle]
-        fn esp_wifi_preempt_current_task(&self) -> *mut c_types::c_void {
-            <$t as $crate::Scheduler>::current_task(&$name)
+        fn esp_wifi_preempt_current_task() -> *mut c_void {
+            <$t as $crate::preempt::Scheduler>::current_task(&$name)
         }
         #[no_mangle]
         fn esp_wifi_preempt_task_create(
-            &self,
-            task: extern "C" fn(*mut c_types::c_void),
-            param: *mut c_types::c_void,
+            task: extern "C" fn(*mut c_void),
+            param: *mut c_void,
             task_stack_size: usize,
-        ) -> *mut c_types::c_void {
-            <$t as $crate::Scheduler>::task_create(&$name, task, param, task_stack_size)
+        ) -> *mut c_void {
+            <$t as $crate::preempt::Scheduler>::task_create(&$name, task, param, task_stack_size)
         }
         #[no_mangle]
-        fn esp_wifi_preempt_schedule_task_deletion(&self, task_handle: *mut c_types::c_void) {
-            <$t as $crate::Scheduler>::schedule_task_deletion(&$name, task_handle)
+        fn esp_wifi_preempt_schedule_task_deletion(task_handle: *mut c_void) {
+            <$t as $crate::preempt::Scheduler>::schedule_task_deletion(&$name, task_handle)
         }
         #[no_mangle]
-        fn esp_wifi_preempt_current_task_thread_semaphore(&self) -> *mut c_types::c_void {
-            <$t as $crate::Scheduler>::current_task_thread_semaphore(&$name)
+        fn esp_wifi_preempt_current_task_thread_semaphore() -> *mut c_void {
+            <$t as $crate::preempt::Scheduler>::current_task_thread_semaphore(&$name)
         }
     };
 }
